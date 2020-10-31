36bee2bc989f340c893a096111d73fa82d3aac6e
Richard Mills <scripts.richard@gmail.com>
2020-10-31 16:00:25 -0600

    add history to local storage

--- src/store/image/mutations.js
+++ src/store/image/mutations.js
@@ -1,8 +1,12 @@
+import { LocalStorage } from 'quasar'
+
 export function insertHistory(state, image) {
   const history = state.history.filter(i => i.id !== image.id)
 
   history.splice(0, 0, image)
   state.history = history.slice(0, 10)
+
+  LocalStorage.set('history', state.history)
 }
 
 export function setImage(state, image) {
--- src/store/image/state.js
+++ src/store/image/state.js
@@ -1,6 +1,10 @@
+import { LocalStorage } from 'quasar'
+
+const history = LocalStorage.has('history') ? LocalStorage.getItem('history') : []
+
 export default function () {
   return {
     image: { alt: undefined, id: undefined, src: undefined, thumb: undefined },
-    history: [],
+    history,
   }
 }
